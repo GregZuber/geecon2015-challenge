b7d0a982399fc405279611e598b33fbdc46b7616
Grzegorz Zuber <zubergrzesiek@gmail.com>
2015-05-14 11:18:19 +0200

    Bug fixes: ReportGenerator.generate RestTransactionRepository RestUserRepostitory

--- src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
+++ src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
@@ -1,9 +1,17 @@
 package pl.allegro.promo.geecon2015.domain;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import pl.allegro.promo.geecon2015.domain.stats.FinancialStatisticsRepository;
+import pl.allegro.promo.geecon2015.domain.stats.FinancialStats;
 import pl.allegro.promo.geecon2015.domain.transaction.TransactionRepository;
+import pl.allegro.promo.geecon2015.domain.transaction.UserTransaction;
+import pl.allegro.promo.geecon2015.domain.transaction.UserTransactions;
+import pl.allegro.promo.geecon2015.domain.user.User;
 import pl.allegro.promo.geecon2015.domain.user.UserRepository;
 
 @Component
@@ -25,8 +33,42 @@ public class ReportGenerator {
     }
 
     public Report generate(ReportRequest request) {
+        FinancialStats financialStats = getFinancialStats(request);
+        return generateReport(financialStats);
+    }
+
+    private FinancialStats getFinancialStats(ReportRequest request) {
+        return financialStatisticsRepository.listUsersWithMinimalIncome(request.getMinimalIncome(), 
+                request.getUsersToCheck());
+    }
+    
+    private Report generateReport(FinancialStats financialStats) {
         Report report = new Report();
+        for (UUID uuid : financialStats){
+            ReportedUser reportedUser = getReportedUser(uuid);
+            report.add(reportedUser);
+        }
         return report;
+    }
+
+    private ReportedUser getReportedUser(UUID uuid) {
+        User user = userRepository.detailsOf(uuid);
+        UserTransactions transactions = transactionRepository.transactionsOf(uuid);
+        BigDecimal transactionsNumber = getTransactionsNumber(transactions);
+        
+        return new ReportedUser(uuid, user.getName(), transactionsNumber);
+    }
+
+    private BigDecimal getTransactionsNumber(UserTransactions transactions) {
+        if (transactions == null) {
+            return null;
+        }
+        
+        BigDecimal transactionsNumber = new BigDecimal(0);
+        for (UserTransaction transaction : transactions.getTransactions()) {
+            transactionsNumber = transactionsNumber.add(transaction.getAmount());
+        }
+        return transactionsNumber;
     }
     
 }
--- src/main/java/pl/allegro/promo/geecon2015/infrastructure/RestUserRepository.java
+++ src/main/java/pl/allegro/promo/geecon2015/infrastructure/RestUserRepository.java
@@ -3,7 +3,9 @@ package pl.allegro.promo.geecon2015.infrastructure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
 import pl.allegro.promo.geecon2015.domain.user.User;
 import pl.allegro.promo.geecon2015.domain.user.UserRepository;
 
@@ -24,6 +26,10 @@ public class RestUserRepository implements UserRepository {
     
     @Override
     public User detailsOf(UUID userId) {
-        return restTemplate.getForEntity(baseUri + "/users/" + userId.toString(), User.class).getBody();
+        try {
+            return restTemplate.getForEntity(baseUri + "/users/" + userId.toString(), User.class).getBody();
+        } catch(HttpServerErrorException e) {
+            return new User(userId, "<failed>");
+        }
     }
 }
